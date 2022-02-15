7b0f0aba50738ab44e39bbc888b2a13e62ced6ed
Arman Danesh <arman@likeablegeek.com>
2022-02-15 19:43:03 +0000

    Added metar library

--- popup.js
+++ popup.js
@@ -7,17 +7,16 @@ chrome.storage.sync.get("color", ({ color }) => {
 
 // When the button is clicked, inject setPageBackgroundColor into current page
 changeColor.addEventListener("click", async () => {
-    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+  const metar = parseMETAR("KCLE 220136Z 31006KT 10SM FEW020 BKN024 OVC049 22/21 A2984 RMK AO2 RAE04 P0000 T02220206");
+
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
   
     chrome.scripting.executeScript({
       target: { tabId: tab.id },
-      function: setPageBackgroundColor,
+      function: () => {
+//        alert(metar.station);
+        $("body").css("background-color","green");
+      },
     });
  });
-  
-  // The body of this function will be executed as a content script inside the current page
-  function setPageBackgroundColor() {
-    chrome.storage.sync.get("color", ({ color }) => {
-      document.body.style.backgroundColor = color;
-    });
-  }
